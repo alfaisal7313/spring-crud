33be2cdf4c64a76c2505f3afb3a6de74d980a878
Muhammad Al Faisal <ma.faisal@eidupay.com>
2021-07-14 10:26:02 +0700

    Configuratioin repository

--- src/main/java/com/crud/service/StudentService.java
+++ src/main/java/com/crud/service/StudentService.java
@@ -1,23 +1,20 @@
 package com.crud.service;
 
 import com.crud.model.Student;
+import com.crud.repository.StudentRepository;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Component
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "Mariam@gmail.com",
-                        LocalDate.of(2000, Month.APRIL, 5),
-                        22
-                )
-        );
+        return studentRepository.findAll();
     }
 }
